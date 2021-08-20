46ca4f908b4a19d36e315c82031db906fd53f8e7
yeji <shfks1212@naver.com>
2021-08-20 12:38:35 +0900

    install json server

--- voca-list/src/components/dayList.jsx
+++ voca-list/src/components/dayList.jsx
@@ -2,7 +2,6 @@ import dummy from '../db/data.json';
 import { Link } from 'react-router-dom';
 
 export default function DayList() {
-  console.log(dummy);
   return (
     <ul className='list_day'>
       {dummy.days.map((day) => (
